81e7fa507d00cd765714de10bca7cc3f6667bba8
Shikohe <131605960+Shikohe@users.noreply.github.com>
2023-06-19 12:53:08 +0400

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///animals.db'
 app.config['SECRET_KEY'] = 'secret'
 db = SQLAlchemy(app)
 
